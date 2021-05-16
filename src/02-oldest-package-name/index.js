40bd1cd80695b1eea9fc8199938694d9a9435126
Thomas-Briody <tom@briody.net>
2021-05-16 14:33:42 +0100

    refactor complete, all tests pass

--- src/02-oldest-package-name/index.js
+++ src/02-oldest-package-name/index.js
@@ -31,21 +31,18 @@ The results should have this structure:
 const axios = require('axios')
 
 module.exports = async function oldestPackageName() {
-  // TODO
 
   const res = await axios.post('http://ambush-api.inyourarea.co.uk/ambush/intercept', {
     "url": "https://api.npms.io/v2/search/suggestions?q=react",
     "method": "GET",
     "return_payload": true
   })
-  // console.log(res.data.content)
-  const allPackages = res.data.content
-  const sortedPackages = allPackages.sort((packageA, packageB) => {
-    // console.log('PACKAGE A DATE', packageA.package.date)
-    const packageADateTimestamp = new Date(packageA.package.date).getTime()
-    const packageBDateTimestamp = new Date(packageB.package.date).getTime()
-    return packageADateTimestamp - packageBDateTimestamp 
+  const sortedPackages = res.data.content.sort((packageA, packageB) => {
+    const packageADTstamp = new Date(packageA.package.date).getTime()
+    const packageBDTstamp = new Date(packageB.package.date).getTime()
+
+    return packageADTstamp - packageBDTstamp 
   })
-  // console.log('SORTED', sortedPackages)
+  
   return sortedPackages[0].package.name
-};
+}
--- src/03-organise-maintainers/index.js
+++ src/03-organise-maintainers/index.js
@@ -41,34 +41,27 @@ The results should have this structure:
 const axios = require('axios')
 
 module.exports = async function organiseMaintainers() {
-  // TODO
 
   const res = await axios.post('http://ambush-api.inyourarea.co.uk/ambush/intercept', {
     "url": "https://api.npms.io/v2/search/suggestions?q=react",
     "method": "GET",
     "return_payload": true
   })
-  
-  const allPackages = res.data.content
 
-
-  const maintainerArray = allPackages.reduce((maintainers, currentPackage) => {
+  const maintainerArray = res.data.content.reduce((maintainers, currentPackage) => {
     currentPackage.package.maintainers.forEach((maintainerObject) => {
       // We need to check if we've already added the maintainer to the maintainersArray
-      // If we have, we need to add the oackagename to the existing entry (and sort it) instead of making a new one
+      // If we have, we need to add the packagename to the existing entry (and sort it) instead of making a new one
       const existingMaintainer = maintainers.find(maintainerItem => {
         return maintainerItem.username === maintainerObject.username
-       })
+      })
       if (existingMaintainer) {
         existingMaintainer.packageNames.push(currentPackage.package.name)
         existingMaintainer.packageNames = existingMaintainer.packageNames.sort()
-
       } else {
       // If the maintainer hasn't already been added, we need to create a new entry for them
         maintainers.push({ username: maintainerObject.username, packageNames: [currentPackage.package.name] })
-
       } 
-
     })
     return maintainers
   }, [])
@@ -76,10 +69,10 @@ module.exports = async function organiseMaintainers() {
   const sortedMaintainerArray = maintainerArray.sort((a, b) => {
     if (a.username < b.username) {
       return -1
-    }
+    } 
     if (a.username > b.username) {
       return +1
-    }
+    } 
     return 0
   })
   // get the username of every maintainer, create an object for eadch maintainer, with a key for their username, and another key
@@ -87,4 +80,4 @@ module.exports = async function organiseMaintainers() {
   // use a reduce to do this for every package
 
   return sortedMaintainerArray
-};
+}
