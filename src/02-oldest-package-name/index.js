c7fb3aec8203f3e09add7c5ad55fe592e266d0c9
Thomas-Briody <tom@briody.net>
2021-05-16 10:53:34 +0100

    tests 1 & 2 working

--- src/02-oldest-package-name/index.js
+++ src/02-oldest-package-name/index.js
@@ -28,8 +28,24 @@ The results should have this structure:
  *  the "name" of the package that has the oldest "date" value
  */
 
+const axios = require('axios')
+
 module.exports = async function oldestPackageName() {
   // TODO
 
-  return name
+  const res = await axios.post('http://ambush-api.inyourarea.co.uk/ambush/intercept', {
+    "url": "https://api.npms.io/v2/search/suggestions?q=react",
+    "method": "GET",
+    "return_payload": true
+  })
+  // console.log(res.data.content)
+  const allPackages = res.data.content
+  const sortedPackages = allPackages.sort((packageA, packageB) => {
+    // console.log('PACKAGE A DATE', packageA.package.date)
+    const packageADateTimestamp = new Date(packageA.package.date).getTime()
+    const packageBDateTimestamp = new Date(packageB.package.date).getTime()
+    return packageADateTimestamp - packageBDateTimestamp 
+  })
+  console.log('SORTED', sortedPackages)
+  return sortedPackages[0].package.name
 };
